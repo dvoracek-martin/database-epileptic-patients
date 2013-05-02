d83151fdc1d216238c3465b670ca50d27d1bc53e
Martin Dvoracek <Dworza@gmail.com>
2013-05-02 23:10:56 +0200

    hibernate support added
    Our project can now work with Hibernate 3.6 Everything should be
    configured well, you just need to download one lib and include it to the
    builder path (if this lib doesn't download automatically via mvn). The
    configuration is set to my localhost database yet and was just for
    testing purposes.

--- Backend/src/main/java/cz/cvut/fit/genepi/controllers/LoginController.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/controllers/LoginController.java
@@ -1,13 +1,18 @@
 package cz.cvut.fit.genepi.controllers;
 
+import java.util.Date;
 import java.util.Locale;
 
+import org.hibernate.classic.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import user.DBUser;
+import util.HibernateUtil;
 
 @Controller
 public class LoginController {
@@ -24,7 +29,19 @@ public class LoginController {
 	public String login(Locale locale, Model model) {
 		logger.info("LOGIN! The client locale is {}.", locale);
 		
-		
+		System.out.println("Maven + Hibernate + Oracle");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+ 
+		session.beginTransaction();
+		DBUser user = new DBUser();
+ 
+		user.setUserId(100);
+		user.setUsername("superman");
+		user.setCreatedBy("system");
+		user.setCreatedDate(new Date());
+ 
+		session.save(user);
+		session.getTransaction().commit();
 		
 		return "login";
 	}	
