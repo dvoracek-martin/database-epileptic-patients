313b41ff886e995f2ba159e13c9eb57daf09e936
Martin Dvoracek <Dworza@gmail.com>
2013-05-08 14:57:49 +0200

    ID param added to POST in PatientOverviewController

--- Backend/src/main/java/cz/cvut/fit/genepi/controllers/PatientOverviewController.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/controllers/PatientOverviewController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import cz.cvut.fit.genepi.models.PatientDAO;
 import cz.cvut.fit.genepi.modelsImpl.PatientDAOImpl;
@@ -19,28 +20,37 @@ public class PatientOverviewController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(PatientOverviewController.class);
 
-	// returns patient from dtb according to id	
+	private int id;
+
+	// returns patient from dtb according to id
 	public PatientDAO findByID(int id) {
 		PatientDAOImpl patientImpl = new PatientDAOImpl();
-		return (PatientDAO) patientImpl.findByID(PatientDAO.class, id);		
+		return (PatientDAO) patientImpl.findByID(PatientDAO.class, id);
 	}
-	
-	
-	// create new Patient	
-	public void createNewPatient( String nin, Date birthday, String gender,
-			int doctorId, int deleted, int checked, int contactId, int commentId){
+
+	// returns current patient from dtb according to id
+	public PatientDAO findByID() {
 		PatientDAOImpl patientImpl = new PatientDAOImpl();
-		patientImpl.createPatient(nin, birthday, gender, doctorId, deleted, checked, contactId, commentId);
+		return (PatientDAO) patientImpl.findByID(PatientDAO.class, this.id);
+	}
+
+	// create new Patient
+	public void createNewPatient(String nin, Date birthday, String gender,
+			int doctorId, int deleted, int checked, int contactId, int commentId) {
+		PatientDAOImpl patientImpl = new PatientDAOImpl();
+		patientImpl.createPatient(nin, birthday, gender, doctorId, deleted,
+				checked, contactId, commentId);
 		patientImpl.save(patientImpl.patient);
 	}
-	
-	
+
 	/**
 	 * selects the profile view to render by returning its name.
-	 */	
+	 */
 
 	@RequestMapping(value = "/patientOverview", method = RequestMethod.POST)
-	public String patientOverviewGET(Locale locale, Model model) {	
+	public String patientOverviewPOST(@RequestParam("id") int id,Locale locale, Model model ) {
+		this.id = id;
+		model.addAttribute("id", id);
 		return "patientOverviewView";
 	}
 
