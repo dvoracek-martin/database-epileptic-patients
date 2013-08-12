cc3d540e1c95c54a190ad6e2a58f2d36c4db9243
Martin Dvoracek <Dworza@gmail.com>
2013-08-12 22:00:08 +0200

    UserDAO + UserDAOImpl implemented

--- Backend/src/main/java/cz/cvut/fit/genepi/DAO/UserDAO.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/DAO/UserDAO.java
@@ -1,5 +1,11 @@
 package cz.cvut.fit.genepi.DAO;
 
-public class UserDAO {
+import java.io.Serializable;
 
+import cz.cvut.fit.genepi.entity.PatientEntity;
+import cz.cvut.fit.genepi.entity.UserEntity;
+
+public interface UserDAO extends GenericDAO<UserEntity, Serializable> {
+
+	UserEntity findUserByID(Integer userID);
 }
--- Backend/src/main/java/cz/cvut/fit/genepi/DAOImpl/UserDAOImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/DAOImpl/UserDAOImpl.java
@@ -1,5 +1,24 @@
 package cz.cvut.fit.genepi.DAOImpl;
 
-public class UserDAOImpl {
+import java.io.Serializable;
 
+import org.hibernate.Query;
+import org.springframework.stereotype.Repository;
+
+import cz.cvut.fit.genepi.DAO.UserDAO;
+import cz.cvut.fit.genepi.entity.UserEntity;
+
+@Repository
+public class UserDAOImpl extends GenericDAOImpl<UserEntity, Serializable>
+		implements UserDAO {
+
+	@Override
+	public UserEntity findUserByID(Integer userID) {
+		UserEntity userEntity;
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"from UserEntity where id = :user_id");
+		query.setParameter("user_id", userID);
+		userEntity = findOne(query);
+		return userEntity;
+	}
 }
