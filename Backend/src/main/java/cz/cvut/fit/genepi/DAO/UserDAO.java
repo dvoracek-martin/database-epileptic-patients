58e380afc61e33126d37004911c56ca854f5bf80
Jan Dušek <dusekja8@fit.cvut.cz>
2013-08-21 14:15:48 +0200

    Some messages rewrote in czech #87

--- Backend/src/main/java/cz/cvut/fit/genepi/DAO/UserDAO.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/DAO/UserDAO.java
@@ -2,7 +2,6 @@ package cz.cvut.fit.genepi.DAO;
 
 import java.io.Serializable;
 
-import cz.cvut.fit.genepi.entity.PatientEntity;
 import cz.cvut.fit.genepi.entity.UserEntity;
 
 public interface UserDAO extends GenericDAO<UserEntity, Serializable> {
