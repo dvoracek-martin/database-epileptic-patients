978e8a47e25b1eefe02f50c5d330bf38c625b42e
Martin Dvoracek <Dworza@gmail.com>
2013-08-25 20:57:13 +0200

    user controller changed

--- Backend/src/main/java/cz/cvut/fit/genepi/controller/UserController.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/controller/UserController.java
@@ -126,11 +126,9 @@ public class UserController {
 	}
 	
 	@RequestMapping(value = "/userEdit", method = RequestMethod.GET)
-	public String userEdit(@ModelAttribute("user") @Valid UserEntity user,
+	public String userEdit(@ModelAttribute("user") UserEntity user,
 			BindingResult result) {
-		if (result.hasErrors()) {
-			return "/userEdit/{"+user.getId()+"}";
-		} else {
+	
 			/*
 			List<UserRoleEntity> listOfAssignedUserRoles=new ArrayList<UserRoleEntity>();		
 			for (RoleEntity i : listOfAssignedRoles){		
@@ -141,7 +139,7 @@ public class UserController {
 			userService.save(user);
 			return "redirect:/userOverview/"
 					+ Integer.toString(user.getId());
-		}
+		
 	}
 
 
