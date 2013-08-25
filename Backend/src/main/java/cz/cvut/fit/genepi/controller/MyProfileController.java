656f758c0d67056edad22c514258aa176fb90b4e
Martin Dvoracek <Dworza@gmail.com>
2013-08-25 04:59:26 +0200

    unused code deleted

--- Backend/src/main/java/cz/cvut/fit/genepi/controller/MyProfileController.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/controller/MyProfileController.java
@@ -1,12 +1,9 @@
 package cz.cvut.fit.genepi.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 
 import javax.validation.Valid;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import cz.cvut.fit.genepi.entity.ContactEntity;
 import cz.cvut.fit.genepi.entity.UserEntity;
 import cz.cvut.fit.genepi.service.UserService;
 
@@ -53,27 +49,6 @@ public class MyProfileController {
 	@RequestMapping(value = "/myProfile", method = RequestMethod.GET)
 	public String myProfileGET(@ModelAttribute("user") @Valid UserEntity user,
 			BindingResult result){
-
-		ContactEntity c = new ContactEntity();
-
-		c.setAddressCity("ADs");
-		c.setAddressCountry("CZ");
-		c.setAddressHn("23524535");
-		c.setAddressPostalcode("23425");
-		c.setAddressStreet("ASDFA");
-		c.setEmail("ASFD@safas.cs");
-		c.setFirstName("ADF");
-		c.setLastName("ADF");
-		c.setPhoneNumber("43255");
-
-		user.setId(0);
-		user.setUsername("sue");
-		user.setPassword(DigestUtils.sha256Hex("suepassword" + "{"
-				+ c.getLastName() + "}"));
-		user.setContact(c);
-
-		userService.save(user);
 		return "myProfileView";
-
 	}
 }
