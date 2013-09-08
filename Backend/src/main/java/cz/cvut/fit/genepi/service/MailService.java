c8e544fc762098bee3ff3a3b51b1f469a5d611f4
Martin Dvoracek <Dworza@gmail.com>
2013-09-09 01:10:40 +0200

    arguments changed, some conditions added

--- Backend/src/main/java/cz/cvut/fit/genepi/service/MailService.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/service/MailService.java
@@ -1,10 +1,10 @@
 package cz.cvut.fit.genepi.service;
 
+import java.util.HashMap;
+
 public interface MailService {
 
-	public void send(String attachmentName, String recipient) throws Exception;
-
-	public void sendMail(String attachmentName, String recipient)
+	public void sendMail(String attachmentName, String recipient, HashMap<String,String> map)
 			throws Exception;
 
 }
