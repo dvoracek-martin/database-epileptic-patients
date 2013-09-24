7bd76c142b7dc1da4abcdc6c4b993bde6150b601
Jan Dusek <YoYo4ever@seznam.cz>
2013-09-25 01:49:48 +0200

    Regexes changed to accept czech characters

--- Backend/src/main/java/cz/cvut/fit/genepi/entity/ContactEntity.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/entity/ContactEntity.java
@@ -26,7 +26,7 @@ public class ContactEntity {
 	private int id;
 
 	/** The first name. */
-	//@Pattern(regexp="[A-Za-z ]*")
+	@Pattern(regexp="[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
 	//@NotNull
 	//@NotBlank
 	@Size(min=1, max=20)
@@ -34,7 +34,7 @@ public class ContactEntity {
 	private String firstName;
 
 	/** The last name. */
-	//@Pattern(regexp="[A-Za-z ]*")
+	@Pattern(regexp="[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
 	//@NotNull
 	//@NotBlank
 	@Size(min=1, max=20)
@@ -42,7 +42,7 @@ public class ContactEntity {
 	private String lastName;
 
 	/** The address street. */	
-	//@Pattern(regexp="[A-Za-z]*")
+	@Pattern(regexp="[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
 	//@NotNull
 	//@NotBlank
 	@Size(min=1, max=50)
@@ -58,7 +58,7 @@ public class ContactEntity {
 	private String addressHn;
 
 	/** The address city. */
-	//@Pattern(regexp="[A-Za-z ]*")
+	@Pattern(regexp="[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
 	//@NotNull
 	//@NotBlank
 	@Size(min=1, max=30)
@@ -74,7 +74,7 @@ public class ContactEntity {
 	private String addressPostalcode;
 
 	/** The address country. */
-	//@Pattern(regexp="[A-Za-z ]*")
+	@Pattern(regexp="[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
 	//@NotNull
 	//@NotBlank
 	@Size(min=1, max=20)
