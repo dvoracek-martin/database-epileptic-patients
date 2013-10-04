58cf806fa6cacecf1062981abec35aa651b87776
Martin Dvoracek <Dworza@gmail.com>
2013-10-04 22:48:47 +0200

    another wave of email fixing

--- Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/MailServiceImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/MailServiceImpl.java
@@ -10,12 +10,16 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Properties;
 
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -130,7 +134,7 @@ public class MailServiceImpl implements MailService {
 						+ " Message about the change of the password was sent to user "
 						+ user.getId());
 			} else if (map.get("subject").equals("creationOfANewUser")) {
-				message.setSubject("New account to GENEPI system");
+				message.setSubject("New account to GENEPI system","utf-8");
 				UserEntity user = (UserEntity) map.get("user");
 				/*
 				 * String text = "Dear " + user.getUsername() + ",\n\n" +
@@ -145,6 +149,37 @@ public class MailServiceImpl implements MailService {
 						messagePrameters, locale);
 				System.out.println(text);
 				message.setText(text, "UTF-8");
+				
+				MimeMessage message2 = new MimeMessage( session ); 
+
+				message2.setFrom(new InternetAddress(from));
+
+				// Set To: header field of the header.
+				message2.addRecipient(Message.RecipientType.TO, new InternetAddress(
+						to));
+
+				
+				
+				// Setting the Subject and Content Type 
+				message2.setSubject( "neco", "utf-8" ); // <---- 
+
+				// Create a message part to represent the body text 
+				BodyPart messageBodyPart = new MimeBodyPart(); 
+				messageBodyPart.setContent( text, "text/html; charset=utf-8" ); // <---- 
+
+				// use a MimeMultipart as we need to handle the file attachments 
+				Multipart multipart = new MimeMultipart(); 
+
+				// add the message body to the mime message 
+				multipart.addBodyPart( messageBodyPart ); 
+
+				// Put all message parts in the message 
+				message2.setContent( multipart ); 
+
+				Transport.send( message2 );
+				
+				
+				
 				Transport.send(message);
 
 				DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
