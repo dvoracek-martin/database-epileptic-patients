d078e291f9fa9fa3e787f3464ed3aba4ec28584f
Martin Dvoracek <Dworza@gmail.com>
2013-10-06 00:20:08 +0200

    some features to xlsx export

--- Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ExportToXlsxServiceImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ExportToXlsxServiceImpl.java
@@ -1,7 +1,13 @@
 package cz.cvut.fit.genepi.serviceImpl;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -17,6 +23,7 @@ import com.itextpdf.text.DocumentException;
 import cz.cvut.fit.genepi.entity.PatientEntity;
 import cz.cvut.fit.genepi.entity.UserEntity;
 import cz.cvut.fit.genepi.service.ExportToXlsxService;
+import cz.cvut.fit.genepi.service.LoggingService;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -25,15 +32,64 @@ import cz.cvut.fit.genepi.service.ExportToXlsxService;
 
 @Service
 public class ExportToXlsxServiceImpl implements ExportToXlsxService {
-	
+
+	private static PatientEntity patient;
+
+	private static UserEntity user;
+
+	private LoggingService logger = new LoggingService();
+
+	private static String getDate() {
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		Date today = Calendar.getInstance().getTime();
+		String reportDate = df.format(today);
+		return reportDate;
+	}
+
 	/**
 	 * Export.
-	 *
-	 * @param pateintID the pateint id
-	 * @throws DocumentException the document exception
-	 * @throws IOException Signals that an I/O exception has occurred.
+	 * 
+	 * @param pateintID
+	 *            the pateint id
+	 * @throws DocumentException
+	 *             the document exception
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
 	 */
-	public void export(PatientEntity patient, UserEntity user, List<String> exports) throws DocumentException, IOException {
+	public void export(PatientEntity patient, UserEntity user,
+			List<String> exports) {
+
+		ExportToXlsxServiceImpl.patient = patient;
+		ExportToXlsxServiceImpl.user = user;
+
+		logger.setLogger(ExportToXlsxServiceImpl.class);
+		writeData();
+	}
+
+	private void writeData() {
+		String downloadFolder = System.getProperty("user.home")
+				+ System.getProperty("file.separator") + "Download_Links"
+				+ System.getProperty("file.separator");
+
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.indexOf("win") >= 0) {
+			downloadFolder.replace("\\", "/");
+		} else {
+			File f = new File(downloadFolder + "patient" + patient.getId()
+					+ ".xlsx");
+			if (!f.getParentFile().exists())
+				f.getParentFile().mkdirs();
+			if (f.exists())
+				f.delete();
+			try {
+				f.createNewFile();
+			} catch (IOException e) {
+				logger.logError(
+						"Couldn't create new file when trying to save xlsx file.",
+						e);
+			}
+		}
+
 		// Blank workbook
 		Workbook wb = new XSSFWorkbook();
 		CreationHelper createHelper = wb.getCreationHelper();
@@ -52,9 +108,28 @@ public class ExportToXlsxServiceImpl implements ExportToXlsxService {
 		row.createCell(3).setCellValue(true);
 
 		// Write the output to a file
-		FileOutputStream fileOut = new FileOutputStream("c:/temp/patient"
-				+ Integer.toString(patient.getId()) + ".xlsx");
-		wb.write(fileOut);
-		fileOut.close();
+		FileOutputStream fileOut = null;
+		try {
+			fileOut = new FileOutputStream(downloadFolder + "patient"
+					+ patient.getId() + ".xlsx");
+		} catch (FileNotFoundException e) {
+			logger.logError("File wasn't found when trying to save xlsx file.",
+					e);
+			e.printStackTrace();
+		}
+		try {
+			wb.write(fileOut);
+		} catch (IOException e) {
+			logger.logError("IO Exception when trying to write to xlsx file.",
+					e);
+			e.printStackTrace();
+		}
+		try {
+			fileOut.close();
+		} catch (IOException e) {
+			logger.logError(
+					"File wasn't found when trying to close xlsx file.", e);
+			e.printStackTrace();
+		}
 	}
 }
