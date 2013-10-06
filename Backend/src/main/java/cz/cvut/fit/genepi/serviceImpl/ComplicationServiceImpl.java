3982884cffa8be32d123f788cebdb46f35d8a2df
Martin Dvoracek <Dworza@gmail.com>
2013-10-06 06:20:58 +0200

    Service anotation added

--- Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ComplicationServiceImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ComplicationServiceImpl.java
@@ -2,9 +2,12 @@ package cz.cvut.fit.genepi.serviceImpl;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Service;
+
 import cz.cvut.fit.genepi.entity.ComplicationEntity;
 import cz.cvut.fit.genepi.service.ComplicationService;
 
+@Service
 public class ComplicationServiceImpl  extends
 GenericServiceImpl<ComplicationEntity, Serializable> implements
 ComplicationService {
