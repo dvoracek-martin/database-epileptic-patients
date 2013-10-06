c29916d754a35b4cab6c3d28e83e75201dcdcedd
Jan Dusek <YoYo4ever@seznam.cz>
2013-10-06 03:26:00 +0200

    autowire of EportParams fixed

--- Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ExportParamsServiceImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/serviceImpl/ExportParamsServiceImpl.java
@@ -2,9 +2,12 @@ package cz.cvut.fit.genepi.serviceImpl;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Service;
+
 import cz.cvut.fit.genepi.entity.ExportParamsEntity;
 import cz.cvut.fit.genepi.service.ExportParamsService;
 
+@Service
 public class ExportParamsServiceImpl extends GenericServiceImpl<ExportParamsEntity, Serializable>
 		implements ExportParamsService {
 
