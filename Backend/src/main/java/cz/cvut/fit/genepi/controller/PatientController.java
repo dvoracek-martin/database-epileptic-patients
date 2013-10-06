e3fb2adc38be88de10691871de03f5d1ecbb2b8e
Martin Dvoracek <Dworza@gmail.com>
2013-10-06 04:56:06 +0200

    dummy array added

--- Backend/src/main/java/cz/cvut/fit/genepi/controller/PatientController.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/controller/PatientController.java
@@ -298,6 +298,7 @@ public class PatientController {
 		model.addAttribute("user", user);
 
 		boolean isReady = false;
+		model.addAttribute("cards", new ArrayList<String>() );
 		model.addAttribute("patient",
 				patientService.findByID(PatientEntity.class, patientID));
 		model.addAttribute("isReady", isReady);
