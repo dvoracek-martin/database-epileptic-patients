4968137d60e6fc6c239bd4dafab2e19ecbb66677
Martin Dvoracek <Dworza@gmail.com>
2013-10-23 02:01:00 +0200

    new util package with new class with set2list and list2set methods

---
+++ Backend/src/main/java/cz/cvut/fit/genepi/util/CollectionConverter.java
@@ -0,0 +1,20 @@
+package cz.cvut.fit.genepi.util;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CollectionConverter<T> {
+
+	public List<T> toList(Set<T> set) {
+		List<T> list = new ArrayList<T>();
+		list.addAll(set);
+		return list;
+	}
+
+	public Set<T> toSet(List<T> list) {
+		Set<T> set = new HashSet<T>(list);
+		return set;
+	}
+}
