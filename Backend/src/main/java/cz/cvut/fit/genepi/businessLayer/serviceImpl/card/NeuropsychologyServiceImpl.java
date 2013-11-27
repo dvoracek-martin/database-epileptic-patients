a822fa1f76e7d45ca3bc52399be826e90c3f79fe
Jan Dusek <YoYo4ever@seznam.cz>
2013-11-27 19:05:57 +0100

    neuropsychology service fixed

--- Backend/src/main/java/cz/cvut/fit/genepi/businessLayer/serviceImpl/card/NeuropsychologyServiceImpl.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/businessLayer/serviceImpl/card/NeuropsychologyServiceImpl.java
@@ -1,11 +1,13 @@
 package cz.cvut.fit.genepi.businessLayer.serviceImpl.card;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import cz.cvut.fit.genepi.businessLayer.service.card.NeuropsychologyService;
 import cz.cvut.fit.genepi.businessLayer.serviceImpl.GenericServiceImpl;
 import cz.cvut.fit.genepi.dataLayer.entity.card.NeuropsychologyEntity;
 
+@Service
 public class NeuropsychologyServiceImpl extends GenericServiceImpl<NeuropsychologyEntity>
 implements NeuropsychologyService{
 
