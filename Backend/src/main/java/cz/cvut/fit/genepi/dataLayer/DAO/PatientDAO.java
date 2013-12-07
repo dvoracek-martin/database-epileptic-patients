2224c727a0fc586704f4d2787e110b1b249aea61
Martin Dvoracek <Dworza@gmail.com>
2013-12-07 04:34:06 +0100

    javadoc in patientDAO updated

--- Backend/src/main/java/cz/cvut/fit/genepi/dataLayer/DAO/PatientDAO.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/dataLayer/DAO/PatientDAO.java
@@ -10,39 +10,124 @@ import cz.cvut.fit.genepi.dataLayer.entity.PatientEntity;
  */
 public interface PatientDAO extends GenericDAO<PatientEntity> {
 
+	/**
+	 * Finds Pateint and all his lists for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithAllLists(int patientId);
 
+	/**
+	 * Finds Pateint and his anamnesis list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithAnamnesisList(int patientId);
 
+	/**
+	 * Finds Pateint and his complication list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithComplicationList(int patientId);
 
+	/**
+	 * Finds Pateint and his diagnosticTestScalpEEG list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithDiagnosticTestScalpEEGList(int patientId);
 
+	/**
+	 * Finds Pateint and his diagnosticTestMRI list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithDiagnosticTestMRIList(int patientId);
 
+	/**
+	 * Finds Pateint and his histology list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithHistologyList(int patientId);
 
+	/**
+	 * Finds Pateint and his InvasiveTestCorticalMapping list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithInvasiveTestCorticalMappingList(
 			int patientId);
 
+	/**
+	 * Finds Pateint and his InvasiveTestECOG list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithInvasiveTestECOGList(int patientId);
 
+	/**
+	 * Finds Pateint and his invasiveTestEEG list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithInvasiveTestEEGList(int patientId);
 
+	/**
+	 * Finds Pateint and his neurologicalFinding list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithNeurologicalFindingList(int patientId);
 
+	/**
+	 * Finds Pateint and his neuropsychology list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithNeuropsychologyList(int patientId);
-	
+
+	/**
+	 * Finds Pateint and his neuropsychologyOld list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithNeuropsychologyOldList(int patientId);
 
+	/**
+	 * Finds Pateint and his operation list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithOperationList(int patientId);
 
+	/**
+	 * Finds Pateint and his outcome list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithOutcomeList(int patientId);
 
+	/**
+	 * Finds Pateint and his pharmacotherapy list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithPharmacotherapyList(int patientId);
 
+	/**
+	 * Finds Pateint and his seizure list for chosen patient
+	 * @param patientID the ID of the pateint
+	 * @return Pateint as the PateintEntity
+	 */
 	public PatientEntity getPatientByIdWithSeizureList(int patientId);
 	
-	public List<PatientEntity> performSearch(AdvancedSearchEntity advancedSearch);
 
+	/**
+	 * Does the searching
+	 * @param advancedSearch as AdvancedSearchEntity
+	 * @return List of PatientEntity
+	 */
+	public List<PatientEntity> performSearch(AdvancedSearchEntity advancedSearch);
 }
