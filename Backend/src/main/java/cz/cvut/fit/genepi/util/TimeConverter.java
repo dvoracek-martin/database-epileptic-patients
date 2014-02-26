df353692ae7f92fb5f9e2228a86b89d2495ad193
Martin Dvoracek <Dworza@gmail.com>
2014-02-26 14:15:29 +0100

    timeconverter for patientEntity

--- Backend/src/main/java/cz/cvut/fit/genepi/util/TimeConverter.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/util/TimeConverter.java
@@ -31,6 +31,24 @@ public class TimeConverter {
         return Integer.toString(age.getYears());
     }
 
+    public static String getCurrentAge(PatientEntity patient) {
+        DateTime birth = new DateTime(patient.getBirthday());
+        DateTime today = new DateTime(Calendar.getInstance().getTime());
+        Years age = Years.yearsBetween(birth.withTimeAtStartOfDay(), today.withTimeAtStartOfDay());
+
+        return Integer.toString(age.getYears());
+    }
+
+    public static String getAgeAtTheBeginningOfEpilepsy(PatientEntity patient) {
+        DateTime birth = new DateTime(patient.getBirthday());
+        if (patient.getAnamnesisList().size() < 1)
+            return "NA";
+        DateTime epilepsy = new DateTime(patient.getAnamnesisList().get(0).getBeginningEpilepsy());
+        Years age = Years.yearsBetween(birth.withTimeAtStartOfDay(), epilepsy.withTimeAtStartOfDay());
+
+        return Integer.toString(age.getYears());
+    }
+
     public static String getDate(Date date) {
         DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         String formatedDate = df.format(date);
