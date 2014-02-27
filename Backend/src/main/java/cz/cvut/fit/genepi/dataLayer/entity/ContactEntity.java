a2e763dab524f16ea29faff98e926da44fbeea3b
Martin Dvoracek <Dworza@gmail.com>
2014-02-27 19:07:22 +0100

    regexes updated

--- Backend/src/main/java/cz/cvut/fit/genepi/dataLayer/entity/ContactEntity.java
+++ Backend/src/main/java/cz/cvut/fit/genepi/dataLayer/entity/ContactEntity.java
@@ -25,27 +25,28 @@ public class ContactEntity {
     /**
      * The first name.
      */
-    @Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
+    @Pattern(regexp = "[^0-9]*")
+    //@Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
     //@NotNull
     //@NotBlank
-    @Size(min = 1, max = 20)
+    @Size(min = 1, max = 50)
     @Column(name = "FIRST_NAME", length = 20, nullable = false)
     private String firstName;
 
     /**
      * The last name.
      */
-    @Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
+    @Pattern(regexp = "[^0-9]*")
+    //@Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
     //@NotNull
     //@NotBlank
-    @Size(min = 1, max = 20)
+    @Size(min = 1, max = 50)
     @Column(name = "LAST_NAME", length = 20, nullable = false)
     private String lastName;
 
     /**
      * The address street.
      */
-    @Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
     //@NotNull
     //@NotBlank
     @Size(min = 1, max = 50)
@@ -55,7 +56,7 @@ public class ContactEntity {
     /**
      * The address hn.
      */
-    @Pattern(regexp = "[0-9]*")
+    @Pattern(regexp = "[0-1]+[0-9]*")
     //@NotNull
     //@NotBlank
     @Size(min = 1, max = 10)
@@ -68,14 +69,14 @@ public class ContactEntity {
     @Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
     //@NotNull
     //@NotBlank
-    @Size(min = 1, max = 30)
+    @Size(min = 1, max = 50)
     @Column(name = "ADDRESS_CITY", length = 30, nullable = true)
     private String addressCity;
 
     /**
      * The address postalcode.
      */
-    @Pattern(regexp = "[0-9]*")
+    @Pattern(regexp = "[1-9]+[0-9]*")
     //@NotNull
     //@NotBlank
     @Size(min = 1, max = 10)
@@ -85,10 +86,9 @@ public class ContactEntity {
     /**
      * The address country.
      */
-    @Pattern(regexp = "[ÁáČčĎďÉéĚěÍíŇňÓóŘřŠšŤťÚúŮůÝýŽžA-Za-z ]*")
     //@NotNull
     //@NotBlank
-    @Size(min = 1, max = 20)
+    @Size(min = 1, max = 50)
     @Column(name = "ADDRESS_COUNTRY", length = 20, nullable = true)
     private String addressCountry;
 
