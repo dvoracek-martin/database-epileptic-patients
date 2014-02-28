4e693695a75846849072cf1c0cd523910867c1ae
Jan Dusek <YoYo4ever@seznam.cz>
2014-02-28 14:38:28 +0100

    port edited

--- Backend/src/main/webapp/resources/custom/js/patient-list.js
+++ Backend/src/main/webapp/resources/custom/js/patient-list.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
 
         $.ajax({
             type: "GET",
-            url: "http://localhost:8080/GENEPI/patient/listSearch",
+            url: "http://localhost:5001/GENEPI/patient/listSearch",
             data: "search=" + searchString + "&maxResults=" + maxResults + "&pageNumber=" + pageNumber,
             success: function (response) {
                 var obj = JSON.parse(response);
